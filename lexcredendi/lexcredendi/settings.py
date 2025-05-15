a9b20538e31b7d8fc36bf5acebeeb830f6d54dfe
delperdang <steve.delperdang@gmail.com>
2025-05-15 04:06:16 +0000

    removed unused environment vars

--- lexcredendi/lexcredendi/settings.py
+++ lexcredendi/lexcredendi/settings.py
@@ -76,11 +76,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "lexcredendi.wsgi.application"
 ASGI_APPLICATION = "lexcredendi.asgi.application"
 
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
 
 DATABASES = {
     "default": {
